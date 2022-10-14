e27d97e88f3dc80ed5dc869f9cbdb149abcb2278
“Michael <michaelbethel789@gmail.com>
2022-10-13 18:30:43 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
-i#include <stdio.h>
+#include <stdio.h>
 /**
- *  *main -is the entry point for the program.
+ * *main -is the entry point for the program.
 **
 **Return:zero if no error, Non-zero value  if errors.
 **/
