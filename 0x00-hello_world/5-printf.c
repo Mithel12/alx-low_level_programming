71e3a523d728e8e80333bd7f10f17784f99cb4b3
“Michael <michaelbethel789@gmail.com>
2022-10-13 18:04:07 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+*main- serves  as an entry point for the program
+*return:By default return zero if, no errors
+*otherwise return non-zero value
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
